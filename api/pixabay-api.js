f5e11ffc69b9d8df779474b96ade75bae9713dce
james <james.777xd@outlook.com>
2024-06-20 13:01:38 -0500

    new API

--- api/pixabay-api.js
+++ api/pixabay-api.js
@@ -1,6 +1,7 @@
 // pixabay-api.js
 
 const PIXABAY_API_KEY = '44452495-f6f145907b4a93aa486086642';
+const API_KEY='44486199-0deb6864182bedf9f1aa7d2d8';
 
 export async function getImages(name) {
     const response = await fetch(`https://pixabay.com/api/?key=${PIXABAY_API_KEY}&q=${name}`);
@@ -11,7 +12,7 @@ export async function getImages(name) {
 }
 
 export async function getIlustrations(name) {
-    const response = await fetch(`https://pixabay.com/api/?key=${PIXABAY_API_KEY}&q=${name}+flag`);
+    const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${name}+flag`);
     const data = await response.json();
     // Filtrar las imágenes de tipo ""
     const photoImages = data.hits.filter(image => image.type === 'illustration');
