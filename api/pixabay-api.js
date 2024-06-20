f2c2be7b81e48caead8470cdef04202378be550e
james <james.777xd@outlook.com>
2024-06-20 13:19:55 -0500

    definition API

--- api/pixabay-api.js
+++ api/pixabay-api.js
@@ -6,18 +6,19 @@ const API_KEY='44486199-0deb6864182bedf9f1aa7d2d8';
 export async function getImages(name) {
     const response = await fetch(`https://pixabay.com/api/?key=${PIXABAY_API_KEY}&q=${name}`);
     const data = await response.json();
-    // Filtrar las imágenes de tipo "photo"
-    const photoImages = data.hits.filter(image => image.type === 'photo');
-    return photoImages;
+    if (data.hits && Array.isArray(data.hits)) {
+        // Filtrar las imágenes de tipo "photo"
+        return data.hits.filter(image => image.type === 'photo');
+    }
+    return [];
 }
 
 export async function getIlustrations(name) {
     const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${name}+flag`);
     const data = await response.json();
-    // Filtrar las imágenes de tipo ""
-    const photoImages = data.hits.filter(image => image.type === 'illustration');
-    return photoImages;
+    if (data.hits && Array.isArray(data.hits)) {
+        // Filtrar las imágenes de tipo "illustration"
+        return data.hits.filter(image => image.type === 'illustration');
+    }
+    return [];
 }
-
-
-      
